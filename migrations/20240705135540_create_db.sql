b2ee3a7fa47530761e29ce72846a980726008f3b
Kirr Gross <kirrgross@gmail.com>
2024-07-05 17:41:31 +0300

    поменял индексы

--- migrations/20240705135540_create_db.sql
+++ migrations/20240705135540_create_db.sql
@@ -16,6 +16,9 @@ CREATE TABLE IF NOT EXISTS tasks(
                                     user_id int,
                                     foreign key (user_id) references users (id) ON DELETE CASCADE
 );
+CREATE INDEX IF NOT EXISTS idx_tasks_user_id ON tasks(user_id);
+CREATE INDEX IF NOT EXISTS idx_users_surname ON users(surname);
+CREATE INDEX IF NOT EXISTS idx_users_passport_number ON users(passport_number);
 -- +goose StatementEnd
 
 
